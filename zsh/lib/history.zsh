2b2a95ee61bc13314d734da8d82a8fbf263cbe07
Steven Humphrey <catchgithub@33k.co.uk>
2016-03-26 14:58:51 +0000

    Restore the bulk of the core settings I had with oh-my-zsh

---
+++ zsh/lib/history.zsh
@@ -0,0 +1,15 @@
+setopt append_history
+setopt extended_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_space
+setopt hist_verify
+setopt inc_append_history
+setopt share_history # share command history data
+
+# Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
+HISTSIZE=1000
+SAVEHIST=1000
+HISTFILE=~/.zsh_history
+
+alias history='fc -il 1'
---
+++ zsh/lib/misc.zsh
@@ -0,0 +1,7 @@
+# Allow comments in interactive shell
+setopt interactivecomments
+
+setopt long_list_jobs
+
+# if I want to delete backwards a directory, don't want / in word
+WORDCHARS=''

8b220ca40aad110208772b19f1ea2ef6bae14556
Steven Humphrey <catchgithub@33k.co.uk>
2024-02-16 14:24:55 +0000

    Simplify pyenv

--- lib/python.sh
+++ lib/python.sh
@@ -1,9 +1,8 @@
 # stop venv from messing with my PS1
 VIRTUAL_ENV_DISABLE_PROMPT=1
 
+# still adds some time to startup
 if command -v pyenv 1>/dev/null 2>&1; then
-  # path
-  eval "$(pyenv init --path)"
-  # completions
+  export PYENV_ROOT="$HOME/.pyenv"
   eval "$(pyenv init -)"
 fi
